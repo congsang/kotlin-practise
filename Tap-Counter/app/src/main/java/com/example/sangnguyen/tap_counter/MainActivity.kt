5a4dbaa00dcbb687efbbef7aa309f00da4a8fd51
Sang <ngcongsang@gmail.com>
2017-12-23 01:04:42 +0700

    first commit

---
+++ Tap-Counter/app/src/main/java/com/example/sangnguyen/tap_counter/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.sangnguyen.tap_counter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var currentNumber = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        // Tap button
+        tapButton.setOnClickListener {
+            currentNumber += 1
+            numberTextView.text = currentNumber.toString()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        this.menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.resetButton -> {
+                this.currentNumber = 0
+                this.numberTextView.text = "0"
+            }
+            else -> println("item not found onOptionsItemSelected")
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
+
